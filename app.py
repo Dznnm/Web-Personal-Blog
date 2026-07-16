e75886f729a1ba687ebdae2f50bdf4f51576560d
Dznnm <danun.speed@gmail.com>
2026-07-16 19:32:17 +0700

    mock homepack

--- app.py
+++ app.py
@@ -5,4 +5,17 @@ app = Flask(__name__)
 #Routes
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    articles = [
+        {"id": 1,
+         "date": "2024-06-01",
+         "title": "My First Blog Post",
+         "content": "This is the content of my first blog post."
+        },
+        {"id": 2,
+            "date": "2024-06-02",
+         "title": "My Second Blog Post",
+         "content": "This is the content of my second blog post."
+        }
+    ]
+
+    return render_template('home.html', articles=articles)
